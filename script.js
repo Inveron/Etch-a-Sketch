a55cef6602a0f1e6ca06f340663ab81273cf7c00
Inveron <michaeldensmorem@gmail.com>
2022-03-11 18:18:34 -0500

    Got the boxes to add, now to make them into a grid

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.querySelector("#squares");
 
+
+
 for (let i = 0; i <= 256; i++) {
+    
+
     let div  = document.createElement('div');
-    div.setAttribute('style', 'border-color: black; background-color: red');
-    container.append(div)
+
+    div.setAttribute('style', 'width: 2%; height: 0; padding-top: 2%; margin: 10px; background-color: black; position: relative')
+    div.textContent = "test";
+    container.appendChild(div)
 }
+
+
+container.setAttribute('style', 'display: flex;')
