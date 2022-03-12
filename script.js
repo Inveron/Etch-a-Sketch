52977f79de8669b53fc5b65d40b4581f63664e08
Inveron <michaeldensmorem@gmail.com>
2022-03-11 22:08:31 -0500

    Got the grid set up along with a event listener and an empty function for it.

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const container = document.querySelector("#squares");
 
 
+function hover () {
+    
+}
 
-for (let i = 0; i <= 256; i++) {
+
+for (let i = 0; i <= 255; i++) {
     
 
     let div  = document.createElement('div');
 
-    div.setAttribute('style', 'width: 2%; height: 0; padding-top: 2%; margin: 10px; background-color: black; position: relative')
-    div.textContent = "test";
+    div.setAttribute('style', 'display: grid; gap: 0; width: 40%; height: 0; padding-top: 40%; margin: 5px; background-color: black; position: relative')
+    div.classList.add("sq");
+    //div.textContent = "test";
+    div.addEventListener('hover', hover());
     container.appendChild(div)
 }
 
 
-container.setAttribute('style', 'display: flex;')
+container.setAttribute('style', 'display: grid; grid-template-columns: repeat(16, 16fr); column-gap: 0; grid-column-gap: 0; gap: 0; justify-content: center;')
